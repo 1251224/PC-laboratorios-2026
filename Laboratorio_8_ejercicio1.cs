9a98d89c0ecebcd3b8ab33076bf37d8f938c0fa5
arfloresgo <157499223+1251224@users.noreply.github.com>
2026-03-11 09:50:18 -0600

    Add files via upload

---
+++ Laboratorio_8_ejercicio1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int aprobados = 0;
+        int reprobados = 0;
+        int suma = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Ingrese la nota del estudiante " + i + ": ");
+            int nota = int.Parse(Console.ReadLine());
+
+            suma = suma + nota;
+
+            if (nota >= 61)
+            {
+                Console.WriteLine("El estudiante APRUEBA");
+                aprobados++;
+            }
+            else
+            {
+                Console.WriteLine("El estudiante REPRUEBA");
+                reprobados++;
+            }
+        }
+
+        double promedio = suma / 10.0;
+
+        Console.WriteLine("\nResultados finales:");
+        Console.WriteLine("Promedio de la clase: " + promedio);
+        Console.WriteLine("Cantidad de aprobados: " + aprobados);
+        Console.WriteLine("Cantidad de reprobados: " + reprobados);
+
+        Console.ReadKey();
+    }
+}
